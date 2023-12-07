c96878d39666d6b521627809283670e4179232a1
Rachel Little <racheldawnlittle@gmail.com>
2023-12-07 16:27:33 -0700

    recommitting changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,46 +3,80 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads,
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10,
+    getValue: getTotalPhotos,
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4,
+    getValue: getTotalTopics,
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng",
+    getValue: getUserWithMostUploads,
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza",
+    getValue: getUserWithLeastUploads,
   },
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false,
+    loading: true,
     focused: null,
+    photos: [],
+    topics: [],
   };
 
   selectPanel(id) {
-    this.setState(previousState => ({
-      focused: previousState.focused !== null ? null : id
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
     }));
+  }
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+    const urlsPromise = ["/api/photos", "/api/topics"].map((url) =>
+      fetch(url).then((response) => response.json())
+    );
+
+    Promise.all(urlsPromise).then(([photos, topics]) => {
+      this.setState({
+        loading: false,
+        photos: photos,
+        topics: topics,
+      });
+    });
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
   }
 
   render() {
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused,
     });
+
     if (this.state.loading) {
       return <Loading />;
     }
@@ -54,10 +88,9 @@ class Dashboard extends Component {
     ).map((panel) => (
       <Panel
         key={panel.id}
-        id={panel.id}
         label={panel.label}
-        value={panel.value}
-        onSelect={event => this.selectPanel(panel.id)}
+        value={panel.getValue(this.state)}
+        onSelect={() => this.selectPanel(panel.id)}
       />
     ));
 
